404a8980bf25f6f82ff4cb8284bfcd147164cdf5
Varun Vinod <58334632+knightlyf@users.noreply.github.com>
2022-03-15 18:02:10 +0530

    Add files via upload

---
+++ JS files/script.js
@@ -0,0 +1,8 @@
+cars = ["Car", "Truck", "Bus", "Motorcycle", "Bicycle", "Train"];
+console.log(cars.reverse())
+console.log(cars.length)
+cars.push("Car2")
+cars.pop(2)
+console.log(cars.sort())
+
+
